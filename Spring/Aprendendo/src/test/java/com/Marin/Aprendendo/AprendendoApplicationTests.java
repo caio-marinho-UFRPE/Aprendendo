c17bd3bec646864284d3401ee81f4f13843e2f45
Valadyne <caio.marinho@ufrpe.br>
2025-12-27 14:07:44 -0300

    Spring - olá mundo

---
+++ Spring/Aprendendo/src/test/java/com/Marin/Aprendendo/AprendendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marin.Aprendendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
