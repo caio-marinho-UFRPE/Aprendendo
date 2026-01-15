c2158bb4e8001853cc5270d8d72a07de3224b641
Valadyne <caio.marinho@ufrpe.br>
2026-01-15 06:56:50 -0300

    Angular - calculadora que funciona + if condicional no resultado!
    botei 4 números pq achei q seria mais interessante assim

--- Angular/conceitos-basicos/src/app/calculadora/calculadora.ts
+++ Angular/conceitos-basicos/src/app/calculadora/calculadora.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss',
 })
 export class Calculadora {
+  num1: number = 0;
+  num2: number = 0;
+  num3: number = 0;
+  num4: number = 0;
+  resultado: number = 0;
 
+  calcularResultado() {
+    console.log("Teste pra saber se tá pegando");
+    this.resultado = this.num1 + this.num2 + this.num3 + this.num4;
+  }
 }
